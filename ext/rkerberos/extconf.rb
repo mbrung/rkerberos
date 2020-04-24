02e302be6ede1b69f177f19086e34772edbf93fb
Michael Brung <mbrung@gmail.com>
2020-04-24 15:11:06 +0200

    Add config path for KRB5 headers.

--- ext/rkerberos/extconf.rb
+++ ext/rkerberos/extconf.rb
@@ -1,25 +1,26 @@
-require 'mkmf'
+require "mkmf"
 
-dir_config('rkerberos', '/usr/local')
+dir_config("rkerberos", "/usr/local")
+dir_config("krb5")
 
-have_header('krb5.h')
-have_library('krb5')
+have_header("krb5.h")
+have_library("krb5")
 
-unless pkg_config('com_err')
-  puts 'warning: com_err not found, usually a dependency for kadm5clnt'
+unless pkg_config("com_err")
+  puts "warning: com_err not found, usually a dependency for kadm5clnt"
 end
 
-if have_header('kadm5/admin.h')
-  have_library('kadm5clnt')
+if have_header("kadm5/admin.h")
+  have_library("kadm5clnt")
 else
   raise "kadm5clnt library not found"
 end
 
-if have_header('kdb.h')
-  have_library('libkdb5')
+if have_header("kdb.h")
+  have_library("libkdb5")
 else
-  raise 'kdb5 library not found'
+  raise "kdb5 library not found"
 end
 
-$CFLAGS << ' -std=c99 -Wall -pedantic'
-create_makefile('rkerberos')
+$CFLAGS << " -std=c99 -Wall -pedantic"
+create_makefile("rkerberos")
